bcac1e633682aa40248d15011daae71d05f977f8
Elia Daniele <65940790+edaniele-tpi@users.noreply.github.com>
2023-01-20 11:20:25 +0100

    first commit

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+import unittest
+#
+class Test(unittest.TestCase):
+  '''
+    Test class
+  '''
+  def test_a(self):
+    '''
+      Test a.
+    '''
+    from about_time import about_time
+    #
+    return
