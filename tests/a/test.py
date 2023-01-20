f2e840181410ac0150cbf55802ab75629d95e2b1
Elia Daniele <65940790+edaniele-tpi@users.noreply.github.com>
2023-01-20 12:26:33 +0100

    debugging workflow

--- tests/a/test.py
+++ tests/a/test.py
@@ -8,7 +8,23 @@ class Test(unittest.TestCase):
     '''
       Test a.
     '''
-    from about_time import about_time
+    import pkg_resources
+    import importlib
+    installed_packages = pkg_resources.working_set
+    installed_packages_list = sorted(
+      ["%s==%s" % (i.key, i.version) for i in installed_packages]
+    )
+    print('\nInstalled packages list:')
+    for p in installed_packages_list:
+      print('\t'+p)
+    print('\nImport all packages:')
+    for i in installed_packages:
+      try:
+        importlib.import_module(i.key)
+      except Exception as e:
+        print('\tException found:\t'+str(e))
+      else:
+        print('\tSuccessfully imported:\t'+i.key)
     #
     return
 #
