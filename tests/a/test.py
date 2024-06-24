c7fca965c4d9984635c960772485bfe8f07b4f4f
Elia Daniele <e.daniele@tpicomposites.com>
2024-06-24 12:23:36 +0200

    Update test.py

--- tests/a/test.py
+++ tests/a/test.py
@@ -10,7 +10,9 @@ class Test(unittest.TestCase):
     '''
     import pkg_resources
     import importlib
-    installed_packages = pkg_resources.working_set
+    import pip
+    installed_packages = pip.get_installed_distributions()
+    # installed_packages = pkg_resources.working_set
     installed_packages_list = sorted(
       ["%s==%s" % (i.key, i.version) for i in installed_packages]
     )
